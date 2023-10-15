c01479f6a99caea89e5814b3fc4bd3efb61d9e13
juliele1 <julie.chen@efrei.net>
2023-10-15 20:41:21 +0200

    tout est ok sauf mapping

--- pages/1_🧠_Preparation_for_the_data.py
+++ pages/1_🧠_Preparation_for_the_data.py
@@ -23,17 +23,6 @@ with st.status("Downloading data...", expanded=True) as status:
 
 st.button('Rerun')
 
-with st.status("Downloading data...", expanded=True) as status:
-    st.write("Searching for data...")
-    time.sleep(2)
-    st.write("Found URL.")
-    time.sleep(1)
-    st.write("Downloading data...")
-    time.sleep(1)
-    status.update(label="Download complete!", state="complete", expanded=False)
-
-st.button('Rerun')
-
 @st.cache_data
 def load_data(nrows):
     data = pd.read_csv("effectifs.csv",sep=";")
--- pages/4_🌍_Mapping.py
+++ pages/4_🌍_Mapping.py
@@ -1,71 +1,39 @@
 import streamlit as st
 import pandas as pd
 import pydeck as pdk
-import requests
 
 st.set_page_config(
     page_title="Mapping",
     page_icon="🌍",
 )
 
-data = pd.read_csv("DATA.csv",sep=",")
+data = pd.read_csv("DATA.csv", sep=",")
+dept = pd.read_csv("dept.csv", sep=",")
 
-niveau_maladie = st.selectbox("Sélectionner le niveau de maladie", ["patho_niv1", "patho_niv2", "patho_niv3"])
+merged_data = pd.merge(data, dept, on="dept")
 
-maladies = data[niveau_maladie].unique()
-maladie_selectionnee = st.selectbox(f"Sélectionner la maladie ({niveau_maladie})", maladies)
+pathology_level = st.selectbox("Select pathology level", ["patho_niv1", "patho_niv2", "patho_niv3"])
+pathology = st.selectbox("Select pathology", data[pathology_level].unique())
 
-# Filtrer les données en fonction du niveau de maladie et de la maladie sélectionnée
-filtered_data = data[data[niveau_maladie] == maladie_selectionnee]
+filtered_data = merged_data[merged_data[pathology_level] == pathology]
 
-# Regrouper les données par région et compter le nombre d'occurrences
-region_counts = filtered_data.groupby("region").size().reset_index()
-region_counts.columns = ["region", "occurrences"]
+occurrence_data = filtered_data.groupby("dept").size().reset_index(name="Occurrence")
 
-# Fonction pour obtenir les coordonnées géographiques à partir d'une adresse
-def get_coordinates(address):
-    url = "https://maps.googleapis.com/maps/api/geocode/json"
-    params = {"address": address, "key": "YOUR_API_KEY"}
-    response = requests.get(url, params=params)
-    if response.status_code == 200:
-        results = response.json()["results"]
-        if len(results) > 0:
-            location = results[0]["geometry"]["location"]
-            return location["lat"], location["lng"]
-    return None
+colonnes_choisies = ["patho_niv1", "patho_niv2", "patho_niv3", "dept", "Latitude", "Longitude"]
 
-# Exemple d'utilisation de la fonction get_coordinates
-address = "1600 Amphitheatre Parkway, Mountain View, CA"
-coordinates = get_coordinates(address)
-if coordinates is not None:
-    lat, lng = coordinates
-    print(f"Latitude: {lat}, Longitude: {lng}")
-else:
-    print("Unable to get coordinates for address:", address)
+merged_data = merged_data[colonnes_choisies]
 
+layer = pdk.Layer(
+    "ScatterplotLayer",
+    data=merged_data,
+    get_position=["Longitude", "Latitude"],
+    get_color="Occurrence",
+    get_radius=5000,
+    pickable=True,
+)
 
-# Afficher la carte avec Pydeck
-st.title("Carte des occurrences de maladies par région")
-st.pydeck_chart(pdk.Deck(
-    initial_view_state=pdk.ViewState(
-        latitude=46.603354,
-        longitude=1.888334,
-        zoom=5,
-        pitch=50,
-    ),
-    layers=[
-        pdk.Layer(
-            "HexagonLayer",
-            data=region_counts,
-            get_position="[longitude, latitude]",
-            radius=100000,
-            elevation_scale=5000,
-            elevation_range=[0, 3000],
-            get_fill_color="[occurrences, occurrences/1000, occurrences/1000, 200]",
-            pickable=True,
-            extruded=True,
-        ),
-    ],
-))
+view = pdk.ViewState(latitude=46.2276, longitude=2.2137, zoom=5, pitch=50)
 
+map = pdk.Deck(layers=[layer], initial_view_state=view)
 
+st.pydeck_chart(map)
